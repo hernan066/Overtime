9b08892a56c182a2eab4399094d45ad12696fd5a
hernangit config --global user.name John <hernan0066@gmail.com>
2022-04-01 14:49:11 -0300

    ProductCard

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -2,26 +2,43 @@ import Image from "next/image";
 import { initialData } from "../../database/products";
 
 export const ProductCard = () => {
-  
-
   return (
-    
-      <div className="cards__container">
-        {initialData.products.map((product) => (
-          <div className="card__container" key={product.slug}>
-            <div className="card__image-container">
+    <div className="cards__container">
+      {initialData.products.map((product) => (
+        <div className="card__container" key={product.slug}>
+          <div className="card__image-container">
+             <div className="card__image-container-img1">
               <Image
                 src={`/products/${product.images[0]}`}
                 alt="img"
-                 width={380}
-                height={380} 
+                width={380}
+                height={380}
+                layout="responsive"
+                objectFit="cover"
+              />
+            </div> 
+            <div className="card__image-container-img2">
+              <Image
+                src={`/products/${product.images[1]}`}
+                alt="img"
+                width={380}
+                height={380}
                 layout="responsive"
                 objectFit="cover"
               />
             </div>
           </div>
-        ))}
-      </div>
-    
+          <div className="card__content">
+            <div className="card__content-row1">
+              <h3 className="card__title">{product.title}</h3>
+              <p className="card__price">${product.price}</p>
+            </div>
+            <div className="card__content-row2">
+              <p className="card__type">{product.type}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -9,6 +9,27 @@ export const Navbar = () => {
 
   return (
     <>
+      <div className="promo-bar__container">
+        <div className="marquee-text-container">
+          <div className="marquee-text-text">
+            <span>
+              Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒  Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒  Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒  Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒  Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒 Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒 Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒 Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒 Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒 Free OT Wristbands on orders $95+ 🎁 Free U.S shipping on orders
+              $75+ 🛒
+            </span>
+          </div>
+         
+        </div>
+      </div>
+
       <div className="header__inner">
         <div className="header__container">
           <div className="header__nav">
