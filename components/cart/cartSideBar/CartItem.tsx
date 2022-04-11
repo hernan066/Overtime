bb0bdc18a0dfa50a7f91a164d23a27182a718af3
hernangit config --global user.name John <hernan0066@gmail.com>
2022-04-11 14:56:02 -0300

    ...

--- components/cart/cartSideBar/CartItem.tsx
+++ components/cart/cartSideBar/CartItem.tsx
@@ -1,72 +1,93 @@
-import Image from "next/image"
-import { FC, useState } from "react"
+import Image from "next/image";
+import { FC, useState, useEffect, useLayoutEffect } from "react";
 import { useDispatch } from "react-redux";
-import { ICartProduct } from "../../../interfaces/cart"
+import { ICartProduct } from "../../../interfaces/cart";
 import { updateQuantity } from "../../../redux/cartSlice";
+import handler from "../../../pages/api/products/index";
 
 interface Props {
-    product: ICartProduct;
+  product: ICartProduct;
 }
 
-
-export const CartItem:FC<Props> = ({product}) => {
+export const CartItem: FC<Props> = ({ product }) => {
   const dispatch = useDispatch();
-const [countProduct, setCountProduct] = useState<number>(1);
-
-
+  const price1 = product.price;
+  const [tempCartProduct, setTempCartProduct] = useState<ICartProduct>({
+    _id: product._id,
+    image: product.image,
+    price: product.price,
+    size: product.size,
+    slug: product.slug,
+    title: product.title,
+    gender: product.gender,
+    type: product.type,
+    quantity: 1,
+  });
 
   const addOrRemoveProduct = (count: number) => {
-    if (countProduct + count >= 1) {
-      setCountProduct((currentCount) => currentCount + count);
-      dispatch(updateQuantity({product, countProduct} ));
-      
-      //falta el dispatch al carrito
-      //falta validar que no sea mayor al stock
+    if (tempCartProduct.quantity + count >= 1) {
+      if (count > 0) {
+        setTempCartProduct((currentProduct) => ({
+          ...currentProduct,
+          quantity: currentProduct.quantity + 1,
+        }));
+      } else {
+        setTempCartProduct((currentProduct) => ({
+          ...currentProduct,
+          quantity: tempCartProduct.quantity - 1,
+        }));
+      }
     }
   };
-  
-    return (
+
+  useEffect(() => {
+    dispatch(updateQuantity(tempCartProduct));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ tempCartProduct]);
+
+  return (
     <div className="cart__item-container">
-            <div className="cart__item-img">
-              <Image
-                src={`/products/${product.image}`}
-                alt={product.title}
-                width={100}
-                height={100}
-              />
-            </div>
-            <div className="cart__item-details">
-              <h4>{product.title}</h4>
-               <span>{product.type}</span> 
+      <div className="cart__item-img">
+        <Image
+          src={`/products/${product.image}`}
+          alt={product.title}
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className="cart__item-details">
+        <h4>{product.title}</h4>
+        <span>{product.type}</span>
 
-              <div className="quantity-selector">
-                <button
-                  className="quantity-selector__btn"
-                  onClick={() => addOrRemoveProduct(-1)}
-                >
-                  -
-                </button>
-                <div className="quantity-selector__number">{countProduct}</div>
-                <button
-                  className="quantity-selector__btn"
-                  onClick={() => addOrRemoveProduct(+1)}
-                >
-                  +
-                </button>
-              </div>
-            </div>
-            <div className="cart__item-aside">
-              <span>${product.price}</span>
-
-              <button className="cart__item-remove-icon">
-                <Image
-                  src="/icons/delete.svg"
-                  alt="trash"
-                  width={20}
-                  height={20}
-                />
-              </button>
-            </div>
+        <div className="quantity-selector">
+          <button
+            className="quantity-selector__btn"
+            onClick={() => {
+              addOrRemoveProduct(-1);
+            }}
+          >
+            -
+          </button>
+          <div className="quantity-selector__number">
+            {tempCartProduct.quantity}
           </div>
-  )
-}
+          <button
+            className="quantity-selector__btn"
+            onClick={() => {
+              addOrRemoveProduct(+1);
+            }}
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <div className="cart__item-aside">
+        <span>${tempCartProduct.price * tempCartProduct.quantity}</span>
+
+        <button className="cart__item-remove-icon">
+          <Image src="/icons/delete.svg" alt="trash" width={20} height={20} />
+        </button>
+      </div>
+    </div>
+  );
+};
--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -21,33 +21,28 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    
     addToCart: (state, action: PayloadAction<ICartProduct>) => {
       state.cart = [...state.cart, action.payload];
       state.numberOfItems = state.numberOfItems + 1;
       state.subTotal = state.subTotal + action.payload.price;
-      state.tax = state.tax + (state.subTotal * 0.1);
+      
       state.total = state.subTotal + state.tax;
 
+     
     },
-    updateQuantity: (state, action: PayloadAction<{product: ICartProduct, quantity: number}>) => {
-      state.cart = state.cart.map(product => {
-        if (product._id === action.payload.product._id) {
-          product.quantity = action.payload.quantity;
-        }
-        return product;
-      });
-      state.numberOfItems = state.cart.reduce((acc, product) => acc + product.quantity, 0);
-      state.subTotal = state.cart.reduce((acc, product) => acc + product.price * product.quantity, 0);
-      state.tax = state.subTotal * 0.1;
-      state.total = state.subTotal + state.tax;
+    updateQuantity: (state,action: PayloadAction<ICartProduct>) => {
+      state.cart = state.cart.filter((item) => item._id !== action.payload._id);
+      state.cart = [...state.cart, action.payload];
+      const totalQuantity = state.cart.reduce((acc, item) => {
+        return acc + item.quantity;
+      }, 0);
       
-        
+      state.numberOfItems = totalQuantity;
+      
+      
     },
-   
   },
 });
-
 
 export const { addToCart, updateQuantity } = cartSlice.actions;
 
