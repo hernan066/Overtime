0a84569791c9245b68b61b224e2946b4a01ce209
hernangit config --global user.name John <hernan0066@gmail.com>
2022-04-15 21:38:49 -0300

    Login-Register

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,24 +1,71 @@
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import shopApi from "../../api/shopApi";
 import { ShopLayout } from "../../components/layout/ShopLayout";
+import { isEmail } from '../../utils/validations';
 
-const loginPage = () => {
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  
+
+  const onLogin = async ({email, password}: Inputs) => {
+    try {
+     
+      const { data } = await shopApi.post('/user/login', { email, password });
+      const { token, user } = data;
+      console.log(token, user);
+    
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
   return (
-    <ShopLayout title={""} pageDescription={""}>
+    <ShopLayout title={"Login Overtime"} pageDescription={""}>
       <div className="login__main">
         <div className="login__container">
           <h1>LOG IN</h1>
-          <form>
+          <form onSubmit={handleSubmit(onLogin)} noValidate>
             <div className="form-control">
-              <input type="text" placeholder="Enter your email address" />
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                {...register("email", {
+                  required: "Is required!",
+                  validate: (value) => isEmail(value)
+
+                })}
+              />
+              <span className="form-error">{errors.email?.message}</span>
             </div>
 
             <div className="form-control">
-              <input type="password" placeholder="Enter your password" />
+              <input
+                type="password"
+                placeholder="Enter your password"
+                {...register("password", {
+                  required: "Is required!",
+                  minLength: { value: 6, message: "Min length is 6" },
+                })}
+              />
+              <span className="form-error">{errors.password?.message}</span>
             </div>
 
-            <button className="btn">Login</button>
+            <button className="btn" type="submit">
+              Login
+            </button>
             <p className="text">
-              Don&apos;t have an account? <a href="#">Register</a>
+              Don&apos;t have an account? <Link href="/auth/register" passHref><a >Register</a></Link> 
             </p>
             <Link href="/auth/register" passHref>
               <a>Forgot your password?</a>
@@ -30,4 +77,4 @@ const loginPage = () => {
   );
 };
 
-export default loginPage;
+export default LoginPage;
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,43 +1,103 @@
 import Link from "next/link";
 import { ShopLayout } from "../../components/layout/ShopLayout";
+import { useForm } from "react-hook-form";
+import shopApi from "../../api/shopApi";
+import { isEmail } from "../../utils/validations";
 
-const registerPage = () => {
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onRegister = async ({ name, email, password }: Inputs) => {
+    console.log(name, email, password);
+    
+    try {
+      const { data } = await shopApi.post("/user/register", {
+        name,
+        email,
+        password,
+      });
+      const { token, user } = data;
+      console.log(token, user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <ShopLayout title={""} pageDescription={""}>
+    <ShopLayout title={"Register Overtime"} pageDescription={""}>
       <div className="login__main">
-      <div className="login__container">
-        <h1>SIGN UP</h1>
+        <div className="login__container">
+          <h1>SIGN UP</h1>
 
-        <p>Create an account for faster checkout.</p>
-        <form>
+          <p>Create an account for faster checkout.</p>
+          
+          <form onSubmit={handleSubmit(onRegister)} noValidate>
             <div className="form-control">
-                <input type="text" placeholder="Enter your email address"/>
-                
+              <input
+                type="text"
+                placeholder="Enter your full name"
+                {...register("name", {
+                  required: "Is required!",
+                  minLength: { value: 2, message: "Min length is 2" },
+                })}
+              />
+              <span className="form-error">{errors.name?.message}</span>
+            </div>
+            <div className="form-control">
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                {...register("email", {
+                  required: "Is required!",
+                  validate: (value) => isEmail(value),
+                })}
+              />
+               <span className="form-error">{errors.email?.message}</span>
             </div>
 
             <div className="form-control">
-                <input type="password" placeholder="Enter your password" />
-                
+              <input
+                type="password"
+                placeholder="Enter your password"
+                {...register("password", {
+                  required: "Is required!",
+                  minLength: { value: 6, message: "Min length is 6" },
+                })}
+              />
+              <span className="form-error">{errors.password?.message}</span>
             </div>
-            <div className="form-control">
-                <input type="password" placeholder="Repeat your password" />
-                
-            </div>
+            {/* <div className="form-control">
+              <input 
+              type="password" 
+              placeholder="Repeat your password" />
+              
+            </div> */}
 
-            <button className="btn">sign up</button>
-            
-            
-            <p className="text">Already have an account? 
-            <Link href="/auth/login" passHref>
-            
-            <a>Log in here</a>
-            </Link>
+            <button className="btn" type="submit">
+              sign up
+            </button>
+
+            <p className="text">
+              Already have an account?
+              <Link href="/auth/login" passHref>
+                <a>Log in here</a>
+              </Link>
             </p>
-        </form>
-    </div>
+          </form>
+        </div>
       </div>
     </ShopLayout>
   );
 };
 
-export default registerPage;
+export default RegisterPage;
