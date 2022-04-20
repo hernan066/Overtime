57ce4fa58c8fd838bd0342f1486d0ebe5e231dc9
hernan066 <hernan0066@gmail.com>
2022-04-19 23:46:11 -0300

    Profile-Page

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,31 +1,43 @@
-import React from 'react'
-import { ShopLayout } from '../../components/layout/ShopLayout';
-import { NextPage } from 'next';
-import { useDispatch } from 'react-redux';
-import { logout } from '../../redux/userSlice';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
+import React from "react";
+import { ShopLayout } from "../../components/layout/ShopLayout";
+import { NextPage } from "next";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/userSlice";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 
 const ProfilePage: NextPage = () => {
-  
   const dispatch = useDispatch();
   const router = useRouter();
-  
+
   const userLogout = () => {
     dispatch(logout());
-    Cookies.remove('token');
-    router.replace('/');
+    Cookies.remove("token");
+    router.replace("/");
+  };
 
-  }
-  
-  
   return (
     <ShopLayout title={"Profile"} pageDescription={""}>
-      <h1>Profile</h1>
-      <button className='btn' onClick={()=> userLogout()}>Log out</button>
+      <div className="profile__container">
+        <h1>My Account</h1>
+        <div className="profile__main">
+          
+          <div className="profile__details">
+          <h2>ACCOUNT DETAILS</h2>
+            <p>Hernan Moneta</p>
+            <button className="btn">Add adresses</button>
+          </div>
+          <div className="profile__bag">
+          <h2>BAGS SECURED</h2>
+            <p>You haven&apos;t placed any orders yet.</p>
+          </div>
+          <button className="btn" onClick={() => userLogout()}>
+            Log out
+          </button>
+        </div>
+      </div>
     </ShopLayout>
-  )
-}
+  );
+};
 
-export default ProfilePage
-
+export default ProfilePage;
