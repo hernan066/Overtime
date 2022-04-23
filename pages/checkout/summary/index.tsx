8a8b9b3aeb867885242cf87797c2ab4fef26874f
hernangit config --global user.name John <hernan0066@gmail.com>
2022-04-23 17:27:52 -0300

    Summary-Page

--- pages/checkout/summary/index.tsx
+++ pages/checkout/summary/index.tsx
@@ -1,11 +1,34 @@
 import { ShopLayout } from '../../../components/layout/ShopLayout';
-
+import Link from "next/link";
 
 const ShippingPage = () => {
   return (
     <ShopLayout title={'Checkout Shipping - Overtime'} pageDescription={'Checkout Shipping - Overtime'}>
 
-        <div>ShippingPage</div>
+<div className="checkout__address__container">
+        <div className="checkout__steps">
+          <Link href={"/checkout/cart"}>
+            <a>
+              <span style={{}}>Cart {"> "}</span>
+            </a>
+          </Link>
+          <Link href={"/checkout/address"}>
+            <a>
+              <span style={{}}>Information {"> "}</span>
+            </a>
+          </Link>
+
+         
+          <span style={{ color: "#ff6600" }}>Summary {"> "}</span>
+          <span style={{ color: "#bbb" }}>Payment</span>
+        </div>
+
+        <h1 className="title">Summary</h1>
+
+        <div className="checkout__address__main">
+          
+        </div>
+      </div>
     </ShopLayout>
     
     
